20316f5f95ed5b1d873ad6a2b8daefaefdd3ee1d
Andrew D'Ambrosio <adambrosio20@gmail.com>
2020-05-12 17:41:54 -0400

    minor changes

--- Develop/app.js
+++ Develop/app.js
@@ -5,7 +5,7 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
@@ -49,6 +49,7 @@ function addMember() {
         } else {
             roleReturn = "office phone number"
         }
+
         inquirer.prompt([
             {
                 type: "input",
@@ -78,7 +79,6 @@ function addMember() {
                 if (addTeamMember === "Yes") {
                     console.log("--------------------------")
                     addMember();
-                    
                 } else {
                     buildTeam();
                 }
